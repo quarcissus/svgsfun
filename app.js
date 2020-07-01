c607ba68a232b2af5cc378dcc21ce855541bcb0b
Gerardp <agerardo1397@gmail.com>
2020-07-01 12:44:50 -0500

    logInScreen

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const moonPath = "M251 5.99999C96 -22 1.99999 82.0002 0.999994 195C-6.13928e-06 308 85 404 251 384C262.964 382.559 136 331.235 136 195C136 58.9999 266.745 8.84428 251 5.99999Z"
 
 const timeline = anime.timeline({
-    duration: 750,
+    duration: 1400,
     easing: "easeInQuad"
 });
 
@@ -9,11 +9,13 @@ timeline.add({
         targets: "#circle",
         d: [{
             value: moonPath
-        }]
+        }],
+        duration: 1200,
 
 
     })
     .add({
         targets: 'svg',
-        rotate: 360
-    }, '-= 550')
+        rotate: 720,
+        duration: 1400,
+    }, '-= 650').add
