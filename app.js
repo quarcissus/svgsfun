c4e4d50a60a7b06e18c6597a0549922a761035f8
Gerardo Acosta Olvera <agerardo1397@gmail.com>
2020-07-01 12:19:12 -0500

    Add files via upload
    practicing with some svgs

---
+++ app.js
@@ -0,0 +1,19 @@
+const moonPath = "M251 5.99999C96 -22 1.99999 82.0002 0.999994 195C-6.13928e-06 308 85 404 251 384C262.964 382.559 136 331.235 136 195C136 58.9999 266.745 8.84428 251 5.99999Z"
+
+const timeline = anime.timeline({
+    duration: 750,
+    easing: "easeInQuad"
+});
+
+timeline.add({
+        targets: "#circle",
+        d: [{
+            value: moonPath
+        }]
+
+
+    })
+    .add({
+        targets: 'svg',
+        rotate: 360
+    }, '-= 550')
